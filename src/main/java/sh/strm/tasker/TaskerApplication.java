ee165c9d6ef5f47067371d65a60d620fc9044d83
OPSXCQ <opsxcq@strm.sh>
2017-08-14 20:00:26 -0300

    readme fix

--- src/main/java/sh/strm/tasker/TaskerApplication.java
+++ src/main/java/sh/strm/tasker/TaskerApplication.java
@@ -4,19 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.spotify.docker.client.DefaultDockerClient;
-
 import sh.strm.tasker.runner.DockerTaskRunner;
 
 @SpringBootApplication
 public class TaskerApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(TaskerApplication.class, args);
 	}
-	
+
 	@Bean
-	public DockerTaskRunner getDefaultDockerClient() throws Exception{
+	public DockerTaskRunner getDefaultDockerClient() throws Exception {
 		return new DockerTaskRunner();
 	}
 }
