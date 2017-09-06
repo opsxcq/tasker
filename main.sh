dc6bf859f7f71290ef510b4c5c4011760ec755f1
Rafael <rafael@thestorm.com.br>
2017-09-06 20:31:18 -0300

    Configuration via environment variables added

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -z "$configuration" ]
+then
+  echo '[+] Using configuration from environment variable'
+  echo "$configuration"
+  echo "$configuration" > /application.yml
+else
+  if [ ! -f '/application.yml' ]
+  then
+    echo '[-] Missing /application.yml , please, configure this application before running it'
+    exit -1
+  fi
+fi
+
+java -Djava.security.egd=file:/dev/./urandom -jar /tasker.jar
