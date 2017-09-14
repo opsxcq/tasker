6a8fbb9f54ae4b2f794f9a302de1387b5bfb47fd
Rafael <rafael@thestorm.com.br>
2017-09-14 15:29:29 -0300

    Some error checking on environment variables

--- src/main/java/sh/strm/tasker/task/DockerTask.java
+++ src/main/java/sh/strm/tasker/task/DockerTask.java
@@ -40,7 +40,22 @@ public class DockerTask extends Task {
 		return environment;
 	}
 
-	public void setEnvironment(String[] environment) {
+	public void setEnvironment(String... environment) {
+		if (environment != null && environment.length > 0) {
+			for (String element : environment) {
+				if (element != null) {
+					if (!element.contains("=")) {
+						throw new IllegalArgumentException("Environment variables must follow the 'Variable=Value' format");
+					}
+					String split[] = element.split("=");
+					// It must be at least 2 tokens, why ? Variables can have '=' too, so we won't want to mess with that.
+					// Also verifies if both tokens contain something
+					if (split.length < 2 || split[0] == null || split[0].length() == 0 || split[1] == null || split[1].length() == 0) {
+						throw new IllegalArgumentException("Environment variables must follow the 'Variable=Value' format");
+					}
+				}
+			}
+		}
 		this.environment = environment;
 	}
 
