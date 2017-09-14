ec4fd2c0e77f6707242d893609de5a83e75d1590
OPSXCQ <opsxcq@strm.sh>
2017-09-14 16:50:34 -0300

    Some workarounds removed

--- src/main/java/sh/strm/tasker/util/DockerUtils.java
+++ src/main/java/sh/strm/tasker/util/DockerUtils.java
@@ -1,7 +1,6 @@
 package sh.strm.tasker.util;
 
 import com.spotify.docker.client.DefaultDockerClient;
-import com.spotify.docker.client.exceptions.ConflictException;
 
 public class DockerUtils {
 
@@ -13,19 +12,8 @@ public class DockerUtils {
 	 * @throws Exception
 	 */
 	public static void removeVolume(String name) throws Exception {
-		boolean done = false;
-		for (int i = 0; i < 50 && !done; i++) {
-			try {
-				DefaultDockerClient docker = DefaultDockerClient.fromEnv().build();
-				docker.removeVolume(name);
-				done = true;
-			} catch (ConflictException e) {
-				Thread.sleep(1000);
-			}
-		}
-		if (!done) {
-			throw new ConflictException("Volume is in use and cannot be removed");
-		}
+		DefaultDockerClient docker = DefaultDockerClient.fromEnv().build();
+		docker.removeVolume(name);
 	}
 
 }
