8368085b032463368f2ee86cc804a5834521364d
Rafael <rafael@thestorm.com.br>
2017-10-05 20:00:40 -0300

    Roads? Where we're going, we don't need roads

--- src/test/java/sh/strm/tasker/setup/NotifierSetupTest.java
+++ src/test/java/sh/strm/tasker/setup/NotifierSetupTest.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetupTest;
+
 import sh.strm.tasker.TaskConfiguration;
 import sh.strm.tasker.notify.EmailNotifier;
 import sh.strm.tasker.notify.NotifierConfiguration;
@@ -40,8 +43,48 @@ public class NotifierSetupTest {
 		setup.init();
 	}
 
-	@Test(expected = IllegalStateException.class)
+	@Test
 	public void testSimpleSetupServerTest() {
+		GreenMail server = new GreenMail(ServerSetupTest.SMTP);
+		String username = "test@server.local";
+		String password = "test";
+		server.setUser(username, username, password);
+		server.start();
+
+		//
+		TaskConfiguration taskConfiguration = new TaskConfiguration();
+		List<DockerTask> tasks = new ArrayList<DockerTask>();
+		DockerTask task = new DockerTask();
+		task.setName("test");
+
+		tasks.add(task);
+		taskConfiguration.setDocker(tasks);
+
+		NotifierConfiguration configuration = new NotifierConfiguration();
+		List<EmailNotifier> emailNotifiers = new ArrayList<EmailNotifier>();
+
+		EmailNotifier emailNotifier = new EmailNotifier();
+		emailNotifier.setTask("test");
+		emailNotifier.setServer("localhost");
+		emailNotifier.setPort(3025);
+		emailNotifier.setUsername(username);
+		emailNotifier.setPassword(password);
+
+		emailNotifiers.add(emailNotifier);
+
+		configuration.setEmail(emailNotifiers);
+
+		NotifierSetup setup = new NotifierSetup();
+
+		setup.setTaskConfiguration(taskConfiguration);
+		setup.setConfiguration(configuration);
+
+		setup.init();
+		server.stop();
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testSimpleSetupServerTestFail() {
 		TaskConfiguration taskConfiguration = new TaskConfiguration();
 		List<DockerTask> tasks = new ArrayList<DockerTask>();
 		DockerTask task = new DockerTask();
@@ -67,6 +110,46 @@ public class NotifierSetupTest {
 		setup.setConfiguration(configuration);
 
 		setup.init();
+	}
+
+	@Test
+	public void testSimpleSetupServerTestLoginError() {
+		GreenMail server = new GreenMail(ServerSetupTest.SMTP);
+		String username = "test@server.local";
+		String password = "test";
+		server.setUser(username, username, password);
+		server.start();
+
+		//
+		TaskConfiguration taskConfiguration = new TaskConfiguration();
+		List<DockerTask> tasks = new ArrayList<DockerTask>();
+		DockerTask task = new DockerTask();
+		task.setName("test");
+
+		tasks.add(task);
+		taskConfiguration.setDocker(tasks);
+
+		NotifierConfiguration configuration = new NotifierConfiguration();
+		List<EmailNotifier> emailNotifiers = new ArrayList<EmailNotifier>();
+
+		EmailNotifier emailNotifier = new EmailNotifier();
+		emailNotifier.setTask("test");
+		emailNotifier.setServer("localhost");
+		emailNotifier.setPort(3025);
+		emailNotifier.setUsername(username);
+		emailNotifier.setPassword(username);
+
+		emailNotifiers.add(emailNotifier);
+
+		configuration.setEmail(emailNotifiers);
+
+		NotifierSetup setup = new NotifierSetup();
+
+		setup.setTaskConfiguration(taskConfiguration);
+		setup.setConfiguration(configuration);
+
+		setup.init();
+		server.stop();
 	}
 
 }
